eff79f4a6529b8b2f4174f68866246411cd06673
endormi <39559256+endormi@users.noreply.github.com>
2023-06-06 21:02:34 +0300

    Quick updates

--- README.md
+++ README.md
@@ -10,11 +10,17 @@ Clone repository:
 git clone https://github.com/endormi/webaimcc-cli.git
 ```
 
-Install `requests` package:
+Install `requests` and `charred` packages:
 
 ```
 python -m pip install requests
+pip install charred
 ```
+
+Default values are (if no input is provided):
+
+- Foreground color: `#000000`.
+- Background color: `#FFFFFF`.
 
 Run app:
 
--- app.py
+++ app.py
@@ -1,39 +1,36 @@
-import requests, signal, sys
+import requests
+import signal
+import sys
+from charred import is_same_char, repeat_char
+
 
 def handler(sig, frame):
     sys.exit()
 
-signal.signal(signal.SIGINT, handler)
 
-def is_same_char(char):
-    num_of_char = len(char)
-    for c in range(1, num_of_char):
-        if char[c] != char[0]:
-            return False
-    return True
+def get_input(prompt, default_value):
+    value = input(prompt).strip()
+    return value or repeat_char(default_value, 6)
 
-def repeat_char(char, num_of_times):
-    return (char * (num_of_times//len(char) + 1))[:num_of_times]
 
 def main():
+    signal.signal(signal.SIGINT, handler)
+
     print("Use hexadecimal (without #).")
-    fcolor = input('Foreground color: ').strip()
-    bcolor = input('Background color: ').strip()
+    fcolor = get_input('Foreground color: ', '0')
+    bcolor = get_input('Background color: ', 'F')
 
-    # Value if empty input
-    if not fcolor: fcolor = repeat_char('0', 6)
-    if not bcolor: bcolor = repeat_char('F', 6)
-
-    # If same character and length is less than 6
-    # API prints out different result when only having 3
-    if is_same_char(fcolor) and len(fcolor) < 6: fcolor = repeat_char(fcolor, 6)
-    if is_same_char(bcolor) and len(bcolor) < 6: bcolor = repeat_char(bcolor, 6)
+    if is_same_char(fcolor) and len(fcolor) < 6:
+        fcolor = repeat_char(fcolor, 6)
+    if is_same_char(bcolor) and len(bcolor) < 6:
+        bcolor = repeat_char(bcolor, 6)
 
     url = 'https://webaim.org/resources/contrastchecker/?fcolor={}&bcolor={}&api'.format(fcolor, bcolor)
 
     response = requests.get(url, timeout=1).json()
     for k, v in response.items():
         print(k, ":", v)
+
 
 if __name__ == '__main__':
     main()
